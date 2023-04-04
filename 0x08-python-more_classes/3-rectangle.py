bd949363b77b825887585de265ec1f0f10136966
Y1AMEER <adeyemiyusuf41@gmail.com>
2023-04-03 20:43:23 -0700

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,52 +1,25 @@
 #!/usr/bin/python3
 """
-This module is composed by a class that defines a Rectangle
+Defines a class Rectangle
 """
 
 
 class Rectangle:
-    """ Class that defines a rectangle """
-
+    """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
-        """ Method that initializes the instance
-        
-        Args:
-            width: rectangle width
-            height: rectangle height
-            
-            
-        """
-
+        """Initializes the rectangle"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ method that returns the value of the width
-        
-        Returns:
-            rectangle width
-            
-            
-        """
-
+        """getter for the private instance attribute width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ method that defines the width
-        
-        Args:
-            value: width
-            
-        Raises:
-            TypeError: if width is not an integer
-            ValueError: if width is less than zero
-            
-            
-        """
-
-        if not isinstance(value, int):
+        """setter for the private instance attribute width"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -54,75 +27,32 @@ class Rectangle:
 
     @property
     def height(self):
-        """ method that returns the value of the height
-        
-        Returns:
-            rectangle height
-            
-            
-        """
-
+        """getter for the private instance attribute height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ method that defines the height
-        
-        Args:
-            value: height
-            
-        Raises:
-            TypeError: if height is not an integer
-            ValueError: if height is less than zero
-            
-            
-        """
-
-        if not isinstance(value, int):
+        """setter for the private instance attribute height"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """ Method that calculates the Rectangle area
-        
-        Returns:
-            rectangle area
-            
-            
-        """
-
-        return self.width * self.height
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
 
     def perimeter(self):
-        """ Method that calculates the Rectangle perimeter
-        
-        Returns:
-            rectangle perimeter
-            
-            
-        """
-
-        if self.width == 0 or self.height == 0:
+        """returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
             return 0
-
-        return (2 * self.width) + (2 * self.height)
+        return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        """ Method that returns the Rectangle #
-        
-        Returns:
-            str of the rectangle
-            
-        """
-
-        rectangle = ""
-
-        if self.width == 0 or self.height == 0:
-            return rectangle
-
-        for i in range(self.height):
-            rectangle += ("#" * self.width) + "\n"
-
-        return rectangle[:-1]
+        """returns printable string representation of the rectangle"""
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#" * self.__width
+                                for j in range(self.__height))
+        return string
